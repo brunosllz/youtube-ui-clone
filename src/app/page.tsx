fe42d6d0a778be29729cbd623e6771861e4e6653
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-02-28 23:13:50 -0300

    feat(home): added html structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,50 @@
-// import { Roboto } from 'next/font/google'
+'use client'
 
-import Image from 'next/image'
 import Link from 'next/link'
+import Image from 'next/image'
 
-// const Roboto = Inter({ subsets: ['latin'] })
+import logo from '../assests/logo.svg'
+import {
+  List,
+  MagnifyingGlass,
+  Microphone,
+  VideoCamera,
+  SquaresFour,
+  Bell,
+} from 'phosphor-react'
 
 export default function Home() {
   return (
     <div>
-      <header>hello world</header>
+      <header>
+        <div>
+          <List />
+          <Image src={logo} alt="youtube" />
+        </div>
+        <div>
+          <form>
+            <input placeholder="Search" />
+            <button>
+              <MagnifyingGlass />
+            </button>
+          </form>
+
+          <button>
+            <Microphone />
+          </button>
+        </div>
+        <div>
+          <VideoCamera />
+          <SquaresFour />
+          <Bell />
+          <Image
+            src="https://www.github.com/brunosllz.png"
+            alt="bruno Luiz"
+            width={24}
+            height={24}
+          />
+        </div>
+      </header>
       <aside>
         <nav>
           <Link href="/">Home</Link>
@@ -37,7 +73,36 @@ export default function Home() {
           </Link>
         </nav>
       </aside>
-      <main></main>
+      <div>
+        <a href="#" />
+        <a href="#" />
+        <a href="#" />
+        <a href="#" />
+        <a href="#" />
+      </div>
+
+      <main>
+        <div>
+          <div></div>
+          <div>
+            <Image
+              src="https://www.github.com/brunosllz.png"
+              alt="bruno Luiz"
+              width={24}
+              height={24}
+            />
+            <div>
+              <strong>
+                Lorem ipsum dolor sit amet, consecte adipiscing elit.
+              </strong>
+              <div>
+                <span>Gouse</span>
+                <span>15K Views .1 week ago</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
