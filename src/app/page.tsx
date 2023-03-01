b50a26aeb95c4aa5a74a9c7815d5852f8862698e
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-03-01 12:57:45 -0300

    styles(home): added header styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,33 +15,41 @@ import {
 
 export default function Home() {
   return (
-    <div>
-      <header>
-        <div>
-          <List />
+    <div className="w-full min-h-screen bg-black">
+      <header className="w-full py-2 px-4 flex items-center justify-between bg-[#212121]">
+        <div className="flex gap-2 items-center">
+          <List size={24} />
           <Image src={logo} alt="youtube" />
         </div>
-        <div>
-          <form>
-            <input placeholder="Search" />
-            <button>
-              <MagnifyingGlass />
-            </button>
-          </form>
+        <div className="flex items-center gap-1 min-w-[470px]">
+          <div className="flex items-center rounded-sm w-full border border-[#303030]">
+            <form className="flex items-center w-full">
+              <input
+                placeholder="Search"
+                className="h-10 w-full bg-[#121212] py-[0.625rem] px-2 placeholder:text-[#AAAAAA]"
+              />
+              <button className="w-16 bg-[#303030] h-10 flex items-center justify-center">
+                <MagnifyingGlass size={18} />
+              </button>
+            </form>
+          </div>
 
-          <button>
-            <Microphone />
+          <button className="p-2 bg-black rounded-full">
+            <Microphone size={18} weight="fill" />
           </button>
         </div>
-        <div>
-          <VideoCamera />
-          <SquaresFour />
-          <Bell />
+        <div className="flex gap-4 items-center">
+          <div className="flex items-center gap-3">
+            <VideoCamera size={24} />
+            <SquaresFour size={24} />
+            <Bell size={24} />
+          </div>
           <Image
             src="https://www.github.com/brunosllz.png"
             alt="bruno Luiz"
-            width={24}
-            height={24}
+            quality={100}
+            width={32}
+            height={32}
           />
         </div>
       </header>
