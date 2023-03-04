c6bdd37293309df42891989db15fffbec3f29691
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-03-03 21:51:05 -0300

    refactor(navlink): added short nav link

--- src/Components/Navlink.tsx
+++ src/Components/Navlink.tsx
@@ -12,34 +12,54 @@ interface NavlinkProps {
   title: string
   children: ReactNode
   href: string
+  isShort?: boolean
 }
 
-export function Navlink({ children, title, href }: NavlinkProps) {
+export function Navlink({ children, title, href, isShort }: NavlinkProps) {
   const path = usePathname()
 
   const currentRoute = path === href
 
   return (
-    <Link
-      href={href}
-      className={clsx(
-        'flex items-center gap-6 py-2 px-6 rounded-md hover:bg-zinc-600',
-        {
-          'bg-zinc-700': currentRoute,
-        },
-      )}
-    >
-      <IconContext.Provider
-        value={{
-          weight: currentRoute ? 'fill' : 'regular',
-        }}
-      >
-        <Slot className={clsx('w-[1.125rem] h-[1.125rem] fill-white')}>
-          {children}
-        </Slot>
-      </IconContext.Provider>
+    <>
+      {isShort ? (
+        <Link
+          href={href}
+          className={clsx(
+            'flex flex-col items-center gap-1 py-4 rounded-md hover:bg-zinc-600',
+          )}
+        >
+          <IconContext.Provider
+            value={{
+              weight: currentRoute ? 'fill' : 'regular',
+            }}
+          >
+            <Slot className={clsx('w-5 h-5 fill-white')}>{children}</Slot>
+          </IconContext.Provider>
 
-      {title}
-    </Link>
+          <span className="text-[0.625rem]">{title}</span>
+        </Link>
+      ) : (
+        <Link
+          href={href}
+          className={clsx(
+            'flex items-center gap-6 py-2 px-6 rounded-md hover:bg-zinc-600',
+            {
+              'bg-zinc-700': currentRoute,
+            },
+          )}
+        >
+          <IconContext.Provider
+            value={{
+              weight: currentRoute ? 'fill' : 'regular',
+            }}
+          >
+            <Slot className={clsx('w-5 h-5 fill-white')}>{children}</Slot>
+          </IconContext.Provider>
+
+          {title}
+        </Link>
+      )}
+    </>
   )
 }
