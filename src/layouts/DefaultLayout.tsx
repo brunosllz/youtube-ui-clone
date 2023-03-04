a6725f154e502232750b7e2d84cd52690008d957
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-03-04 12:39:13 -0300

    refactor(defaultLayout): adjust position for children components

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -14,9 +14,14 @@ export function DefaultLayout({ children }: DefaultLayoutProps) {
   return (
     <SidebarContextProvider>
       <Header />
-      <div className="flex pt-[3.6rem]">
-        <Sidebar />
-        {children}
+      <Sidebar />
+
+      <div className="flex flex-[1_1_auto] overflow-hidden pt-[3.625rem] pl-[240px]">
+        <div className="flex flex-[1_1_auto] overflow-hidden">
+          <div className="flex flex-[1_1_auto] h-full overflow-auto">
+            {children}
+          </div>
+        </div>
       </div>
     </SidebarContextProvider>
   )
